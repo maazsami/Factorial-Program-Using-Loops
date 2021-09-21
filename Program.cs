067af1f7b2626fc5b5c82e29394c304343193f56
maazsami <maazsamifaisal@gmail.com>
2021-09-21 13:07:53 -0700

    mesaage

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Factorial_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1;
+          
+
+            for (int i = 1; i <= 5; i++)
+            {
+                fact = fact * i;  
+            }
+
+            Console.WriteLine(fact);
+            Console.ReadLine();
+        }
+    }
+}
